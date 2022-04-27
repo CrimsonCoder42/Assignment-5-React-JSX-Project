980c77cada6b96f54feadabb2e56386711192910
CrimsonCoder42 <nostro37@gmail.com>
2022-04-27 09:56:59 -0400

    Guesses made and Numbers tried locked in

--- number_guess/src/App.js
+++ number_guess/src/App.js
@@ -3,20 +3,24 @@ import './App.css';
 
 
 
+let gameNum = Math.floor(Math.round * 100) + 1
+
 function App() {
 
-  const [playerNum, setPlayerNum] = useState("");
+  const [gameNumber, setGameNumber] = useState();
+  const [playerNum, setPlayerNum] = useState(" ");
   const [playerCount, setPlayerCount] = useState(0);
   const [numPlayed, setNumPlayed] = useState([]);
-
-  const buttonPress = ( ) => { 
-    setPlayerCount(playerCount + 1)
-    setNumPlayed([...numPlayed], playerNum)
-  } 
 
   const playerGuessed = (n) => {
     setPlayerNum(n.target.value)
   }
+
+  const buttonPress = () => { 
+    setPlayerCount(playerCount + 1)
+    setNumPlayed([...numPlayed, playerNum])
+  } 
+  
   return (
     <div className="App">
       <h1> Guess a number </h1>
@@ -27,17 +31,15 @@ function App() {
       <div className="stats">
       
        <p> Guesses made: {playerCount} </p>
-       <p> 
-        Numbers tried: 
-        {numPlayed.map((item, index) => {
-          return <p key={index}>{item}</p>
-          
-        })}
-        </p>
+       <p> Numbers tried:  </p>
+       <ul> {numPlayed.map((item, index) => {
+          return <li> {item} </li>
+          })} </ul>
+       
+    
       </div>
-
     </div>
-  );
-}
+)}
+
 
 export default App;
